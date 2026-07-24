20a99342cac276219fb84be4e308eb5bc3cb21cc
svyatikpro <sviatoslav362@gmail.com>
2026-07-24 13:30:21 +0200

    import fix

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from database import get_session
